b7e991412356b117f05d827c352c8eafc25f889e
hroque01 <hadson.roque01@gmail.com>
2022-11-29 13:02:45 +0100

    Push obj to an array

---
+++ main.js
@@ -0,0 +1,35 @@
+const { createApp } = Vue
+
+createApp({
+  data() {
+    return {
+        error: false,
+        newTask: '',
+        tasks: [
+            {
+                text: 'Fare i compiti', 
+                done: false
+            },
+            {
+                text: 'Fare la spesa', 
+                done: true
+            },
+            {
+                text: 'Fare il bucato', 
+                done: false
+            }
+        ]
+    }
+  },
+  methods: {
+    addTask(){
+        if (this.newTask.length < 5 || this.newTask === ''){
+
+        } else {
+            this.tasks.push({ text: this.newTask }); 
+        }
+           
+    }
+  }
+}).mount('#app')
+
